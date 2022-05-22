5a8ad9e4f84a6f7a623f40387c8df9589af5c75d
MarkC11235 <96099532+MarkC11235@users.noreply.github.com>
2022-05-22 17:02:23 -0500

    Add files via upload

--- webapp.js
+++ webapp.js
@@ -41,55 +41,55 @@ app.get("/contact", function (request, response) {
     response.render('contact.ejs');
 });
 app.get("/patchnotes", function (request, response) {
-    response.render('patchnotes.ejs');
+    response.render('patchNotes.ejs');
 });
 
 //games 
-app.get("/2048", function (request, response) {
+app.get("/2048-", function (request, response) {
     response.render('2048.ejs');
 });
-app.get("/Bird", function (request, response) {
+app.get("/Bird-", function (request, response) {
     response.render('bird.ejs');
 });
-app.get("/BlockStack", function (request, response) {
+app.get("/BlockStack-", function (request, response) {
     response.render('blockStack.ejs');
 });
-app.get("/CubeDash", function (request, response) {
+app.get("/CubeDash-", function (request, response) {
     response.render('cubeDash.ejs');
 });
-app.get("/HangMan", function (request, response) {
+app.get("/HangMan-", function (request, response) {
     response.render('hangMan.ejs');
 });
-app.get("/MineSweeper", function (request, response) {
+app.get("/MineSweeper-", function (request, response) {
     response.render('minesweeper.ejs');
 });
-app.get("/Memory", function (request, response) {
+app.get("/Memory-", function (request, response) {
     response.render('memory.ejs');
 });
-app.get("/MeteorShower", function (request, response) {
+app.get("/MeteorShower-", function (request, response) {
     response.render('meteorShower.ejs');
 });
-app.get("/IdleGame", function (request, response) {
+app.get("/IdleGame-", function (request, response) {
     response.render('muffinMaker.ejs');
 });
-app.get("/Pong", function (request, response) {
+app.get("/Pong-", function (request, response) {
     response.render('pong.ejs');
 });
-app.get("/Rocket", function (request, response) {
+app.get("/Rocket-", function (request, response) {
     response.render('rocket.ejs');
 });
-app.get("/Serpent", function (request, response) {
+app.get("/Serpent-", function (request, response) {
     response.render('snake.ejs');
 });
-app.get("/Platformer", function (request, response) {
+app.get("/Platformer-", function (request, response) {
     response.render('superJumpMan.ejs');
 });
-app.get("/TargetPractice", function (request, response) {
+app.get("/TargetPractice-", function (request, response) {
     response.render('targetPractice.ejs');
 });
-app.get("/TowerBuilder", function (request, response) {
+app.get("/TowerBuilder-", function (request, response) {
     response.render('towerBuilder.ejs');
 });
-app.get("/ZombieSurvival", function (request, response) {
+app.get("/ZombieSurvival-", function (request, response) {
     response.render('zombieSurvival.ejs');
 });
