95f9858a39cd3f4e602226e8bbca3026bc60d280
MarkC11235 <96099532+MarkC11235@users.noreply.github.com>
2022-07-08 15:56:47 -0500

    Add files via upload

--- db.js
+++ db.js
@@ -1,14 +1,16 @@
 module.exports.db = function DB(app){
     const mysql = require('mysql');
 
-    const con = mysql.createConnection({
+    const pool = mysql.createPool({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         port : process.env.DB_PORT,
-        database: process.env.DB_NAME
+        database: process.env.DB_NAME,
+        connectionLimit: process.env.DB_CONNECTION_LIMIT
     });
 
+    /*
     con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
@@ -22,63 +24,21 @@ module.exports.db = function DB(app){
     });
     //con.end();
     });
+    */
 
     //register and login 
     app.post('/login', function(request, response) {
-	// Capture the input fields
 	let username = request.body.username;
 	let password = request.body.password;
     let keepLoggedIn = request.body.keepLoggedIn;
-	// Ensure the input fields exists and are not empty
-	if (username && password) {
-		// Execute SQL query that'll select the account from the database based on the specified username and password
-		con.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-			// If there is an issue with the query, output the error
-			if (error) throw error;
-			// If the account exists
-			if (results.length > 0) {
-                request.session.username = username;
-                request.session.password = password;
-                request.session.loggedin = true;
-                if(keepLoggedIn){
-                    response.cookie('username', username, {secure : "auto", maxAge : 1000 * 60 * 60 * 24 * 365});
-                    response.cookie('password', password, {secure : "auto", maxAge : 1000 * 60 * 60 * 24 * 365});
-                }
-				response.redirect('/');
-			} else {
-				response.redirect('/error');
-			}			
-		});
-	} else {
-		response.redirect('/error');
-	}
-    });
 
-    app.post('/register', function(request, response) {
-    // Capture the input fields
-    let username = request.body.username;
-    let password = request.body.password;
-    let email = request.body.email;
-    let keepLoggedIn = request.body.keepLoggedIn;
-    // Ensure the input fields exists and are not empty
-    if (username && password && email) {
-        // Execute SQL query that'll select the account from the database based on the specified username and password
-        con.query('SELECT * FROM accounts WHERE username = ? OR email = ?', [username, email], function(error, results, fields) {
-            // If there is an issue with the query, output the error
-            if (error){
-                console.log("Error: " + error);
-                throw error;
-            } 
-            // If the account exists
-            else if (results.length > 0) {
-                console.log("Account already exists");
-                response.redirect('/error');
-            } else {
-                // Execute SQL query that'll insert the account into the database
-                con.query('INSERT INTO accounts (username, password, email) VALUES (?, ?, ?)', [username, password, email], function(error, results, fields) {
-                    // If there is an issue with the query, output the error
-                    if (error) throw error;
-                    // Redirect to home page
+	if (username && password) {
+        pool.getConnection(function(err, con) {
+		    con.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {	
+			    if (error)
+                    throw error;
+
+			    if (results.length > 0) {
                     request.session.username = username;
                     request.session.password = password;
                     request.session.loggedin = true;
@@ -86,14 +46,60 @@ module.exports.db = function DB(app){
                         response.cookie('username', username, {secure : "auto", maxAge : 1000 * 60 * 60 * 24 * 365});
                         response.cookie('password', password, {secure : "auto", maxAge : 1000 * 60 * 60 * 24 * 365});
                     }
-                    console.log("Account created");
-                    response.redirect('/');
-                });
-            }
+				    response.redirect('/');
+			    } 
+                else {
+			    	response.redirect('/error');
+			    }			
+		    });
+            con.release();
         });
-    } else {
-        response.redirect('/error');
-    }
+	}
+    else {
+		response.redirect('/error');
+	}
+
+    });
+
+    app.post('/register', function(request, response) {
+    let username = request.body.username;
+    let password = request.body.password;
+    let email = request.body.email;
+    let keepLoggedIn = request.body.keepLoggedIn;
+
+    if (username && password && email) {
+        pool.getConnection(function(err, con) {
+            con.query('SELECT * FROM accounts WHERE username = ? OR email = ?', [username, email], function(error, results, fields) {
+                if (error){
+                    console.log("Error: " + error);
+                    throw error;
+                }  
+                else if (results.length > 0) {
+                    console.log("Account already exists");
+                    response.redirect('/error');
+                } else {
+                    con.query('INSERT INTO accounts (username, password, email) VALUES (?, ?, ?)', [username, password, email], function(error, results, fields) {
+                    
+                        if (error) throw error;
+                    
+                        request.session.username = username;
+                        request.session.password = password;
+                        request.session.loggedin = true;
+                        if(keepLoggedIn){
+                            response.cookie('username', username, {secure : "auto", maxAge : 1000 * 60 * 60 * 24 * 365});
+                            response.cookie('password', password, {secure : "auto", maxAge : 1000 * 60 * 60 * 24 * 365});
+                        }
+                        console.log("Account created");
+                        response.redirect('/');
+                    });
+                }
+            });
+            con.release();
+        });
+        }
+        else {
+            response.redirect('/error');
+        }
     });
 
 
@@ -102,19 +108,21 @@ module.exports.db = function DB(app){
         var path = request.path;
         path = path.substring(1);
         if(request.session.loggedin){
-            con.query('CREATE TABLE IF NOT EXISTS ' + path + ' (username VARCHAR(255), password VARCHAR(255), data VARCHAR(255), UNIQUE(username))', function (err, result) {
-                if (err) throw err;
-                //console.log("Table created");
-            });
-            con.query('SELECT * FROM ' + path + ' WHERE username = ? AND password = ?', [request.session.username, request.session.password], function(error, results, fields) {
-                if (error) throw error;
-                // If the account exists
-                if (results.length > 0) {
-                    response.json(results);
-                } else {
-                    response.json([]);
-                }
-                response.end();
+            pool.getConnection(function(err, con) {
+                con.query('CREATE TABLE IF NOT EXISTS ' + path + ' (username VARCHAR(255), password VARCHAR(255), data VARCHAR(255), UNIQUE(username))', function (err, results) {
+                    if (err) throw err;
+                });
+                con.query('SELECT * FROM ' + path + ' WHERE username = ? AND password = ?', [request.session.username, request.session.password], function(error, results, fields) {
+                    if (error) throw error;
+                    // If the account exists
+                    if (results.length > 0) {
+                        response.json(results);
+                    } else {
+                        response.json([]);
+                    }
+                    response.end();
+                });
+                con.release();
             });
         }
         else{
@@ -127,30 +135,31 @@ module.exports.db = function DB(app){
         var path = request.path;
         path = path.substring(1);
         if(request.session.loggedin){
-            con.query('CREATE TABLE IF NOT EXISTS ' + path + ' (username VARCHAR(255), password VARCHAR(255), data VARCHAR(255), UNIQUE(username))', function (err, result) {
-                if (err) throw err;
-                //console.log("Table created");
-            });
-            con.query('SELECT * FROM ' + path + ' WHERE username = ? AND password = ?',
-            [request.session.username, request.session.password], function(error, results, fields) {
-                if (error) throw error;
-                // If the account exists
-                if (results.length > 0) {
-                    con.query('UPDATE ' + path + ' SET data = ? WHERE username = ? AND password = ?',
-                    [request.body.data, request.session.username, request.session.password], function(error, results, fields) {
-                        if (error) throw error;
-                        //console.log("Data updated" + path + " " + request.body.data);
-                        response.end();
-                    });
-                }
-                else{
-                    con.query('INSERT INTO '+path+' (username, password, data) VALUES (?, ?, ?)',
-                     [request.session.username, request.session.password, request.body.data], function(error, results, fields) {
-                        if (error) throw error;
-                        //console.log("Data inserted" + path);
-                        response.end();
-                    });
-                }
+            pool.getConnection(function(err, con) {
+                con.query('CREATE TABLE IF NOT EXISTS ' + path + ' (username VARCHAR(255), password VARCHAR(255), data VARCHAR(255), UNIQUE(username))', function (err, result) {
+                    if (err) 
+                        throw err;     
+                });
+                con.query('SELECT * FROM ' + path + ' WHERE username = ? AND password = ?', [request.session.username, request.session.password], function(error, results, fields) {
+                    if (error) 
+                        throw error;
+
+                    if (results.length > 0) {
+                        con.query('UPDATE ' + path + ' SET data = ? WHERE username = ? AND password = ?', [request.body.data, request.session.username, request.session.password], function(error, results, fields) {
+                            if (error) 
+                                throw error;  
+                            response.end();
+                        });
+                    }
+                    else{
+                        con.query('INSERT INTO '+path+' (username, password, data) VALUES (?, ?, ?)', [request.session.username, request.session.password, request.body.data], function(error, results, fields) {
+                            if (error) 
+                                throw error;
+                            response.end();
+                        });
+                    }
+                });
+                con.release();
             });
         }
         else{
@@ -161,6 +170,9 @@ module.exports.db = function DB(app){
     //get data
     app.get('/BlockStackData', function(request, response) {
         getGameData(request, response); 
+    });
+    app.get('/MemoryData', function(request, response) {
+        getGameData(request, response);
     });
     app.get('/MeteorShowerData', function(request, response) {
         getGameData(request, response); //*
@@ -179,6 +191,9 @@ module.exports.db = function DB(app){
     app.post('/BlockStackData', function(request, response) {
         postGameData(request, response);
     });
+    app.post('/MemoryData', function(request, response) {
+        postGameData(request, response);
+    });
     app.post('/MeteorShowerData', function(request, response) {
         postGameData(request, response);
     });
@@ -191,63 +206,4 @@ module.exports.db = function DB(app){
     app.post('/TowerBuilderData', function(request, response) {
         postGameData(request, response);
     });
-
-    //memory game
-    app.get("/MemoryHighScore", function (request, response) {
-        if(request.session.loggedin == true)
-        {
-            con.query("CREATE TABLE IF NOT EXISTS MEMORYHS (username VARCHAR(255), password VARCHAR(255), highscore VARCHAR(50), UNIQUE(username))", function (err, result) {
-                if (err) throw err;
-                //console.log("Table created");
-            });
-            con.query('SELECT * FROM MEMORYHS WHERE username = ? AND password = ?', [request.session.username,request.session.password], function (error, results, fields) {
-                if (error) 
-                    throw error;
-
-                if(results.length > 0)
-                {
-                    //console.log(results);
-                    response.json(results);
-                }
-                else
-                {
-                    var highscore = 9999;
-                    response.json(highscore);
-                }
-            });
-        }
-    });
-    app.post("/MemoryHighScore", function (request, response) {
-        if(request.session.loggedin == true)
-        {
-            con.query("CREATE TABLE IF NOT EXISTS MEMORYHS (username VARCHAR(255), password VARCHAR(255), highscore VARCHAR(50), UNIQUE(username))", function (err, result) {
-                if (err) throw err;
-                //console.log("Table created");
-            });
-            con.query('SELECT * FROM MEMORYHS WHERE username = ? AND password = ?', 
-            [request.session.username,request.session.password], function (error, results, fields) {
-                if (error) 
-                    throw error;
-
-                if(results.length > 0){
-                    con.query('UPDATE MEMORYHS SET highscore = ? WHERE username = ? AND password = ?',
-                    [request.body.highscore, request.session.username, request.session.password],
-                    function (error, results, fields) {
-                        if (error) throw error;
-                        //console.log("Highscore updated " + request.body.highscore);
-                    });
-                }
-                else{
-                    con.query('INSERT INTO MEMORYHS (username, password, highscore) VALUES (?, ?, ?)',
-                    [request.session.username, request.session.password, request.body.highscore],
-                    function (error, results, fields) {
-                        if (error) throw error;
-                        //console.log("Highscore updated " + request.body.highscore);
-                    });
-                }
-            });
-            
-        }
-    });
-
 }
